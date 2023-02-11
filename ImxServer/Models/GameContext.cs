4aba1cddb11866ca2ef72c1225510205c4bda6d4
eddy <be@be-technology.fr>
2023-02-11 21:44:50 +0000

    update data model to add monster

--- ImxServer/Models/GameContext.cs
+++ ImxServer/Models/GameContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Nethereum.Model;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Configuration;
+using System.Security.Policy;
 
 namespace ImxServer.Models
 {
@@ -10,6 +12,10 @@ namespace ImxServer.Models
     {
         private readonly IConfiguration _configuration;
         public DbSet<Player> Players { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<MonsterMove> MonsterMoves { get; set; }
+        public DbSet<Move> Moves { get; set; }
+        public DbSet<Monster> Monsters { get; set; }
 
         public GameContext(IConfiguration configuration)
         {
@@ -35,7 +41,7 @@ namespace ImxServer.Models
     public class Player
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public int PlayerId { get; set; }
         public string Account { get; set; }
         public string Name { get; set; }
         public bool RegisterImx { get; set; }
@@ -45,26 +51,31 @@ namespace ImxServer.Models
     public class Token
     {
         public int TokenId { get; set; }
+        public int MonsterId { get; set; }
+        public Monster Monster { get; set; }
+        public int Level { get; set; }
     }
 
+    [Index(nameof(TokenId), nameof(MoveId), IsUnique  = true)]
+    public class MonsterMove
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MonsterMoveId { get; set; }
+        public int TokenId { get; set; }
+        public Token Token { get; set; }
+        public int MoveId { get; set; }
+        public Move Move { get; set; }
+    }
 
     public class Monster
     {
         public int MonsterId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public int Element { get; set; }
-        public string AtkName1 { get; set; }
-        public string AtkName2 { get; set; }
     }
 
-    public class Item
+    public class Move
     {
-        public int ItemId { get; set; }
+        public int MoveId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public int ItemType { get; set; }
-
     }
-
 }
