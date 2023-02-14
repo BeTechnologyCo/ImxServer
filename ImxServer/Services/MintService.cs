85ae91f8ad183ecc78d00c0fb5b08459a68b9d26
Youtpout <EddyBoughioul@hotmail.fr>
2023-02-14 22:19:47 +0100

    restore appsettings

--- ImxServer/Services/MintService.cs
+++ ImxServer/Services/MintService.cs
@@ -89,7 +89,7 @@ namespace ImxServer.Services
 
             using (var client = new HttpClient())
             {
-                var url = _config["MintService"] +"/api/transfer";
+                var url = _config["MintService"] + "/api/transfer";
 
                 var response = await client.PostAsJsonAsync(url, infos);
 
